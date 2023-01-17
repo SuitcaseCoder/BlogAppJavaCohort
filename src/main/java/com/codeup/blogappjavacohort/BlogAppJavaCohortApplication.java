9523a32ed573a9fb671c867f2fc76c8ecddb6356
laura <laura@codeup.com>
2023-01-17 09:34:03 -0600

    first commit

---
+++ src/main/java/com/codeup/blogappjavacohort/BlogAppJavaCohortApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.blogappjavacohort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppJavaCohortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppJavaCohortApplication.class, args);
+    }
+
+}
